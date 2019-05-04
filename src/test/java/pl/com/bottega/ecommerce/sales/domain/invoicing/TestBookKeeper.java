5ba67d105600544ab09fb5359e1f67e848c5d67c
pallineczka <paulina.stelmasiak97@gmail.com>
2019-05-04 13:44:55 +0200

    isolation testTwoElementInvoiceCalledCalculateTaxTwoTimes

--- src/test/java/pl/com/bottega/ecommerce/sales/domain/invoicing/TestBookKeeper.java
+++ src/test/java/pl/com/bottega/ecommerce/sales/domain/invoicing/TestBookKeeper.java
@@ -10,18 +10,19 @@ import pl.com.bottega.ecommerce.sharedkernel.Money;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.*;
 import static org.mockito.Mockito.when;
 
 public class TestBookKeeper {
 
+    Id id = Id.generate();
+    ClientData clientData = new ClientData(id, "Client");
+    InvoiceRequest invoiceRequest = new InvoiceRequest(clientData);
+    InvoiceFactory invoiceFactory = new InvoiceFactory();
+    BookKeeper bookKeeper = new BookKeeper(invoiceFactory);
+
     @Test
     void testOneElementInvoice(){
-        Id id = Id.generate();
-        ClientData clientData = new ClientData(id, "Client");
-        InvoiceRequest invoiceRequest = new InvoiceRequest(clientData);
-        InvoiceFactory invoiceFactory = new InvoiceFactory();
-        BookKeeper bookKeeper = new BookKeeper(invoiceFactory);
-
         TaxPolicy tax = mock(TaxPolicy.class);
         ProductData productData = mock(ProductData.class);
 
@@ -34,5 +35,22 @@ public class TestBookKeeper {
         Invoice invoice = bookKeeper.issuance(invoiceRequest, tax);
 
         assertThat(invoice.getItems().size(), is(1));
+    }
+
+    @Test
+    void testTwoElementInvoiceCalledCalculateTaxTwoTimes(){
+        TaxPolicy tax = mock(TaxPolicy.class);
+        ProductData productData = mock(ProductData.class);
+
+        when(tax.calculateTax(ProductType.STANDARD, new Money(5))).thenReturn(new Tax(new Money(5), "23%"));
+        when(productData.getType()).thenReturn(ProductType.STANDARD);
+
+        RequestItem requestItem = new RequestItem(productData, 2, new Money(5));
+        invoiceRequest.add(requestItem);
+        invoiceRequest.add(requestItem);
+
+        bookKeeper.issuance(invoiceRequest, tax);
+
+        verify(tax, times(2)).calculateTax(ProductType.STANDARD, new Money(5));
     }
 }
